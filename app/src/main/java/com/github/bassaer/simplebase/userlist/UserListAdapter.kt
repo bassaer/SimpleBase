346b7ad88d172d9283f23bd850dc08c7251c1d44
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-03-06 23:01:27 +0900

    add cell

---
+++ app/src/main/java/com/github/bassaer/simplebase/userlist/UserListAdapter.kt
@@ -0,0 +1,32 @@
+package com.github.bassaer.simplebase.userlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.github.bassaer.simplebase.R
+import com.github.bassaer.simplebase.data.User
+
+
+class UserListAdapter(private val userList: MutableList<User>):
+    RecyclerView.Adapter<UserListAdapter.UserViewHolder>(){
+
+    class UserViewHolder(cell: View): RecyclerView.ViewHolder(cell) {
+        var userView: TextView = cell.findViewById(R.id.cell_name)
+        var countView: TextView = cell.findViewById(R.id.cell_count)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+
+        val cell = LayoutInflater.from(parent.context).inflate(R.layout.user_cell, parent, false)
+        return UserViewHolder(cell)
+    }
+
+    override fun getItemCount(): Int = userList.size
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.userView.text = userList[position].name
+        holder.countView.text = userList[position].count.toString()
+    }
+}
--- app/src/main/java/com/github/bassaer/simplebase/userlist/UserListFragment.kt
+++ app/src/main/java/com/github/bassaer/simplebase/userlist/UserListFragment.kt
@@ -6,15 +6,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.github.bassaer.simplebase.R
+import com.github.bassaer.simplebase.data.User
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class UserListFragment: Fragment(), NewUserDialogFragment.NoticeDialogListener {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var users: MutableList<User>
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.userlist_frag, container, false)
-        val userListView: RecyclerView = root.findViewById(R.id.user_list)
+        users = arrayListOf(User(0, "user1", 0))
+        viewAdapter = UserListAdapter(users)
+        viewManager = LinearLayoutManager(requireContext())
+        recyclerView = root.findViewById<RecyclerView>(R.id.user_list).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+            addItemDecoration(DividerItemDecoration(requireContext(), DividerItemDecoration.VERTICAL))
+        }
         val fab: FloatingActionButton? = activity?.findViewById(R.id.new_user_fab)
         fab?.setOnClickListener {
             activity?.let {
@@ -26,7 +42,9 @@ class UserListFragment: Fragment(), NewUserDialogFragment.NoticeDialogListener {
         return root
     }
     override fun onClickPositiveButton(input: String) {
-        Toast.makeText(requireContext(), input, Toast.LENGTH_SHORT).show()
+        users.add(User(users.size, input, 0))
+        viewAdapter.notifyDataSetChanged()
+        Toast.makeText(requireContext(), getString(R.string.ok_message), Toast.LENGTH_SHORT).show()
     }
 
 }
